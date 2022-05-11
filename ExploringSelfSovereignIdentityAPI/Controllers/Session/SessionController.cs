817b9e6eeadfef84763a9b4c747f9c7837d6f5e7
MxolisiMkanzi <u18059326@tuks.co.za>
2022-05-11 08:06:37 +0200

    Backend session controller setup

---
+++ ExploringSelfSovereignIdentityAPI/Controllers/Session/SessionController.cs
@@ -0,0 +1,51 @@
+﻿using ExploringSelfSovereignIdentityAPI.Commands.Example;
+using ExploringSelfSovereignIdentityAPI.Models.Example;
+using ExploringSelfSovereignIdentityAPI.Queries.Example;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace ExploringSelfSovereignIdentityAPI.Controllers.Session
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class SessionController : Controller
+    {
+        private readonly IMediator mediator;
+
+        public SessionController(IMediator med)
+        {
+            mediator = med;
+        }
+
+        [HttpPost("init")]
+        public IActionResult init()
+        {
+            //return otp
+
+            return Ok();
+        }
+
+        [HttpPost("connect")]
+        public IActionResult validateOTP()
+        {
+            //Accept the OTP
+            //Return the required set of fields
+            //Should redirect to the page where they have to check/select the fields the Holder wants to expose
+
+            return Ok();
+        }
+
+        [HttpPost("confirm")]
+        public IActionResult confirm()
+        {
+            //Receive the selected fields  to expose
+            //Finalise the identity creation
+            //Return the fileds to expose and a token
+
+            return Ok();
+        }
+
+
+    }
+}
