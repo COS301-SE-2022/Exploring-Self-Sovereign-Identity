cdd06776476eba9b9ed2c8132fee68f783713a5f
MxolisiMkanzi <u18059326@tuks.co.za>
2022-05-11 20:28:21 +0200

    ✅ (api) Implement CQRS for session identity confirmation

---
+++ ExploringSelfSovereignIdentityAPI/Models/Response/DefaultIdentityResponse.cs
@@ -0,0 +1,15 @@
+﻿using ExploringSelfSovereignIdentityAPI.Models.DefaultIdentity;
+
+namespace ExploringSelfSovereignIdentityAPI.Models.Response
+{
+    public class DefaultIdentityResponse
+    {
+        public DefaultIdentityModel identity { get; set; }
+        public string token { get; set; }
+
+        public DefaultIdentityResponse()
+        {
+            identity = new DefaultIdentityModel();
+        }
+    }
+}
