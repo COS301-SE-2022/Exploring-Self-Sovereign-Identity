e7a8bfd79b27b3b9a89ff184eb37f94ee28baca8
Johan Smit <jsjohansmit6@gmail.com>
2022-05-11 09:47:09 +0200

    🏷️Implemented SessionRepository Interface

--- ExploringSelfSovereignIdentityAPI/Repositories/SessionRepository/SessionRepository.cs
+++ ExploringSelfSovereignIdentityAPI/Repositories/SessionRepository/SessionRepository.cs
@@ -1,9 +1,27 @@
-﻿namespace ExploringSelfSovereignIdentityAPI.Repositories.SessionRepository
+﻿using ExploringSelfSovereignIdentityAPI.Models.DefaultIdentity;
+using System.Threading.Tasks;
+
+namespace ExploringSelfSovereignIdentityAPI.Repositories.SessionRepository
 {
     public class SessionRepository : ISessionRepository
     {
         private readonly ISessionRepository _sessionRepository;
 
-           
+        public async Task<DefaultIdentityModel> Add(DefaultIdentityModel e)
+        {
+            await Task.FromResult(e);
+            return e;
+        }
+
+        public async Task<DefaultIdentityModel> Update(DefaultIdentityModel e)
+        {
+            e.addAttribute("Name", true);
+            e.addAttribute("Surame", false);
+            e.addAttribute("Email", true);
+            e.addAttribute("Number", false);
+
+            await Task.FromResult(e);
+            return e;
+        }
     }
 }
