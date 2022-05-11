9264b718f0792f8118191635b8e19ca6b3f7d0ee
Johan Smit <jsjohansmit6@gmail.com>
2022-05-11 09:24:10 +0200

    🏷️Added Model For Default Identity

---
+++ ExploringSelfSovereignIdentityAPI/Models/DefaultIdentity/DefaultIdentityModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ExploringSelfSovereignIdentityAPI.Models.DefaultIdentity
+{
+    public class DefaultIdentityModel
+    {
+        public List<Tuple<string, bool>> attributes;
+
+        public DefaultIdentityModel()
+        {
+            attributes = new List<Tuple<string, bool>>();
+        }
+
+        public void addAttribute(string attribute, bool isRequired)
+        {
+            for (int i = 0; i < attributes.Count; i++)
+            {
+                if (attributes[i].Item1 == attribute)
+                    throw new ArgumentException();
+            }
+
+            attributes.Add(new Tuple<string, bool>(attribute, isRequired));
+        }
+
+    }
+}
