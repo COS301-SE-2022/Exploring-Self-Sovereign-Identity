8f353261a010b003a33c88b4544db5d5bbf1e387
Johan Smit <jsjohansmit6@gmail.com>
2022-05-12 19:33:18 +0200

    🔥Added Endpoint testing File

---
+++ ExploringSelfSovereignIdentityAPIUnitTests/Session/SessionControllerUnitTest1.cs
@@ -0,0 +1,35 @@
+﻿using ExploringSelfSovereignIdentityAPI.Commands.SessionCommand;
+using ExploringSelfSovereignIdentityAPI.Controllers.Session;
+using ExploringSelfSovereignIdentityAPI.Models.Default;
+using MediatR;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ExploringSelfSovereignIdentityAPIUnitTests.Session
+{
+    public class SessionControllerUnitTest1
+    {
+        [Fact]
+        public async Task SessionControllerTest()
+        {
+            /*var mockMediator = new Mock<IMediator>();
+            mockMediator.Setup(m => m.Send(It.IsAny<GetDefaultSessionCommand>(), default(CancellationToken)));
+
+
+            var command = new GetDefaultSessionCommand("654321");
+
+            var controller = new SessionController(mockMediator.Object);
+
+            var result = await controller.validateOTP(command);
+            Assert.NotNull(result);
+            Assert.Equal("Default Identity", result.IdentityName);*/
+                
+        }
+    }
+}
