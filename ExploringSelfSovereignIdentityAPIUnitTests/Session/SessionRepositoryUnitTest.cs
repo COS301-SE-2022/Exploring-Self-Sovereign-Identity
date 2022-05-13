37d4b7b1259dfac1bdcec588a4d026b54040d00c
Johan Smit <jsjohansmit6@gmail.com>
2022-05-13 02:16:18 +0200

    Added Unit Tests

--- ExploringSelfSovereignIdentityAPIUnitTests/Session/SessionRepositoryUnitTest.cs
+++ ExploringSelfSovereignIdentityAPIUnitTests/Session/SessionRepositoryUnitTest.cs
@@ -1,4 +1,5 @@
 using ExploringSelfSovereignIdentityAPI.Models.Default;
+using ExploringSelfSovereignIdentityAPI.Models.Response;
 using ExploringSelfSovereignIdentityAPI.Repositories.SessionRepository;
 using System.Threading.Tasks;
 using Xunit;
@@ -18,10 +19,18 @@ namespace ExploringSelfSovereignIdentityAPIUnitTests
         [Fact]
         public async void Get_ValidtSession()
         {
-            DefaultSessionModel defaultSessionModel = await _sessionRepository.GetMockSession(new DefaultSessionModel());
+            var defaultSessionModel = await _sessionRepository.GetMockSession(new DefaultSessionModel());
 
             Assert.Equal(11111, defaultSessionModel.SessionId);
+            Assert.Equal("654321", defaultSessionModel.otp);
+        }
 
+        [Fact]
+        public async void Get_ValidOtpResponse()
+        {
+            var otpResponse = await _sessionRepository.GetOtpResponse(new OtpResponse());
+
+            Assert.Equal("654321", otpResponse.otp);
         }
     }
 }
