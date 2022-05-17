6c14540b715bae79b7be79df3a917288f5245e4f
Henri <40567224+H-e-n-r-i@users.noreply.github.com>
2022-05-18 00:15:51 +0200

    finished primative home page prototype and approve permissions page

--- ExploringSelfSovereignIdentityClient/src/app/app.module.ts
+++ ExploringSelfSovereignIdentityClient/src/app/app.module.ts
@@ -1,13 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+
+
+//materials
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDivider, MatDividerModule } from '@angular/material/divider';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
 import { HomeComponent } from './home/home.component';
 import { SessionConnectComponent } from './session-connect/session-connect.component';
 import { UserProfileComponent } from './user-profile/user-profile.component';
+import { AcceptPermissionsComponent } from './accept-permissions/accept-permissions.component';
 
 @NgModule({
   declarations: [
@@ -15,6 +25,7 @@ import { UserProfileComponent } from './user-profile/user-profile.component';
     HomeComponent,
     SessionConnectComponent,
     UserProfileComponent,
+    AcceptPermissionsComponent,
   ],
   imports: [
     BrowserModule,
@@ -23,6 +34,11 @@ import { UserProfileComponent } from './user-profile/user-profile.component';
     BrowserModule,
     ReactiveFormsModule,
     FormsModule,
+    MatCardModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatDividerModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
