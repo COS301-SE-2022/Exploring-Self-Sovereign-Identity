bab4b562e83c0277c4c4ece9d4d17439fa0cbd10
Henri <40567224+H-e-n-r-i@users.noreply.github.com>
2022-05-27 16:59:45 +0200

    reset base.css, added icon component and IconButton component

--- SSIVueJsClient/src/views/LoginView.vue
+++ SSIVueJsClient/src/views/LoginView.vue
@@ -1,12 +1,28 @@
 <script lang="ts">
+import { MDCRipple } from "@material/ripple";
+import IconBiometric from "../components/icons/IconBiometric.vue";
+import IconButton from "../components/Buttons/IconButton.vue";
 export default {
   data() {
     return {};
   },
-  props: ["text"],
+  components: { IconBiometric, IconButton },
 };
 </script>
 
 <template>
-  <button>{{ text }}</button>
+  <div>
+    <IconButton><IconBiometric /></IconButton>
+  </div>
 </template>
+
+<style lang="scss">
+@use "@material/button/styles";
+
+div {
+  display: block;
+  position: absolute;
+  margin-left: auto;
+  margin-right: auto;
+}
+</style>
