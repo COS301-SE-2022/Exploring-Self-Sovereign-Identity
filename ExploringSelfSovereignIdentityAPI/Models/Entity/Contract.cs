060a107aa8136abbe4cc2d23d731477750cdd5c7
MxolisiMkanzi <u18059326@tuks.co.za>
2022-06-03 21:02:54 +0200

    Add Contract Entities

---
+++ ExploringSelfSovereignIdentityAPI/Models/Entity/Contract.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace ExploringSelfSovereignIdentityAPI.Models.Entity
+{
+    public class Contract
+    {
+        public Guid Id { get; set; }
+        public string Signature { get; set; }
+    }
+}
---
+++ ExploringSelfSovereignIdentityAPI/Models/Entity/ContractAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExploringSelfSovereignIdentityAPI.Models.Entity
+{
+    public class ContractAttribute
+    {
+        public Guid Id { get; set; }
+        public Guid ContractId { get; set; }
+
+        public Guid attributeId { get; set; }
+    }
+}
