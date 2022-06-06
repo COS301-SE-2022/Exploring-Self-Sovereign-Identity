3c0749f487be6075c7bfc1c191bd529a43f1e726
MxolisiMkanzi <u18059326@tuks.co.za>
2022-06-06 20:47:28 +0200

    Add organization entity

--- ExploringSelfSovereignIdentityAPI/Data/ApplicationDbContext.cs
+++ ExploringSelfSovereignIdentityAPI/Data/ApplicationDbContext.cs
@@ -13,31 +13,36 @@ namespace ExploringSelfSovereignIdentityAPI.Data
             Database.EnsureCreated();
         }
 
-        public DbSet<DefaultSessionModel> defaultSessionModels { get; set; }
+        public DbSet<DefaultSessionModel> DefaultSessionModels { get; set; }
         public DbSet<DefaultIdentityModel> DefaultIdentityModels { get; set; }
 
-        public DbSet<UserDataModel> userDataModels { get; set; }
+        public DbSet<UserDataModel> UserDataModels { get; set; }
 
-        public DbSet<Attribute> attributes { get; set; }
+        public DbSet<Attribute> Attributes { get; set; }
 
         public DbSet<OrganizationCredentials> OrganizationCredentials { get; set; } 
+
+        public DbSet<Organization> Organizations { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<DefaultSessionModel>().ToTable("defaultSessionModel");
-            modelBuilder.Entity<DefaultSessionModel>().HasKey("SessionId");
+            modelBuilder.Entity<DefaultSessionModel>().HasKey("Id");
 
             modelBuilder.Entity<DefaultIdentityModel>().ToTable("DefaultIdentityModel");
-            modelBuilder.Entity<DefaultIdentityModel>().HasKey("IdentityName");
+            modelBuilder.Entity<DefaultIdentityModel>().HasKey("Id");
 
-            modelBuilder.Entity<UserDataModel>().ToTable("UserDataModel");
-            modelBuilder.Entity<UserDataModel>().HasKey("UserID");
+            modelBuilder.Entity<UserDataModel>().ToTable("User");
+            modelBuilder.Entity<UserDataModel>().HasKey("Id");
 
             modelBuilder.Entity<OrganizationCredentials>().ToTable("OrganizationCredentials");
-            modelBuilder.Entity<OrganizationCredentials>().HasKey("ID");
+            modelBuilder.Entity<OrganizationCredentials>().HasKey("Id");
 
             modelBuilder.Entity<Attribute>().ToTable("Attribute");
-            modelBuilder.Entity<Attribute>().HasKey("Value");
+            modelBuilder.Entity<Attribute>().HasKey("Id");
+
+            modelBuilder.Entity<Organization>().ToTable("Organization");
+            modelBuilder.Entity<Organization>().HasKey("Id");
 
 
 
