846afe4e937925a16eaff0268e3154e9985eddfe
Rebecca Pillay <u17016534@up.ac.za>
2022-06-07 23:00:41 +0200

    User Data Endpoint (Delete)

---
+++ ExploringSelfSovereignIdentityAPI/Queries/ConnectEndpoint/ConnectEndpointDeleteUserDataQuery.cs
@@ -0,0 +1,14 @@
+﻿using ExploringSelfSovereignIdentityAPI.Models.Entity;
+using MediatR;
+
+namespace ExploringSelfSovereignIdentityAPI.Queries.ConnectEndpoint
+{
+    public class ConnectEndpointDeleteUserDataQuery
+    {
+        public record ConnectEndpointDeleteDataQuery : IRequest<UserDataModel>
+        {
+
+        }
+
+    }
+}
