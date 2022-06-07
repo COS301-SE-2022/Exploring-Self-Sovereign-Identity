16b0301b75865735b2e54e8e60fa4acdb16799ca
Henri <40567224+H-e-n-r-i@users.noreply.github.com>
2022-06-07 15:17:28 +0200

    Updated build error and currently implemeneting home page

--- SSIVueJsClient/src/App.vue
+++ SSIVueJsClient/src/App.vue
@@ -1,13 +1,15 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from "vue-router";
-// import HomeView from "@/views/HomeView.vue";
+import { Calendar } from "@element-plus/icons-vue";
+
+import HomeView from "@/views/HomeView.vue";
 </script>
 
 <template>
   <!-- <LogingView /> -->
   <!-- <h1>SSI</h1> -->
-  <RouterView />
-  <!-- <HomeView /> -->
+  <HomeView />
+  <!-- <RouterView /> -->
 </template>
 
 <style lang="scss"></style>
--- SSIVueJsClient/src/views/HomeView.vue
+++ SSIVueJsClient/src/views/HomeView.vue
@@ -1,17 +1,53 @@
 <script lang="ts">
-import ImageCard from "../components/cards/ImageCard.vue";
+import IconHome from "../components/icons/IconHome.vue";
 export default {
   data() {
     return {
       user: "Peter",
     };
   },
-  components: { ImageCard },
+  components: { IconHome },
 };
 </script>
 
 <template>
-  <ImageCard />
+  <el-card class="card">
+    <template #header>
+      <div>
+        <h1>Temp</h1>
+      </div>
+    </template>
+    <div v-for="o in 4" :key="o" class="text item">{{ "List item " + o }}</div>
+  </el-card>
+
+  <!-- *Bottom navbar -->
+  <div class="nav">
+    <el-menu
+      default-active="1"
+      class="el-menu-vertical-demo"
+      @open="handleOpen"
+      @close="handleClose"
+    >
+      <el-row :gutter="9">
+        <el-col :span="3">
+          <el-menu-item index="1">
+            <el-icon><IconHome /></el-icon>
+          </el-menu-item>
+        </el-col>
+      </el-row>
+    </el-menu>
+  </div>
 </template>
 
-<style lang="scss"></style>
+<style lang="scss">
+.nav {
+  width: 100%;
+  position: fixed;
+  bottom: 0%;
+}
+
+.card {
+  margin: 8px;
+  margin-top: 0;
+}
+</style>
