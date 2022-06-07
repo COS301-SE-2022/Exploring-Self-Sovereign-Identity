3aab52e39da1ca63e0601bf7c5d67097f2f462bd
Rebecca Pillay <u17016534@up.ac.za>
2022-06-07 20:01:09 +0200

    Latex Comments

--- ExploringSelfSovereignIdentityAPI/Repositories/IUserDataRepository.cs
+++ ExploringSelfSovereignIdentityAPI/Repositories/IUserDataRepository.cs
@@ -3,13 +3,38 @@ using ExploringSelfSovereignIdentityAPI.Models.Response;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
-
+//! UserDataRepository 
+/*
+    Created User Data Repository for User Database
+    @author Rebecca Pillay
+ 
+*/
 namespace ExploringSelfSovereignIdentityAPI.Repositories.UserDataRepository
 {
     public interface IUserDataRepository
     {
+        //! GetUserData Function
+        /*
+           To get user data for table:
+
+           Name
+           Surname
+           Email
+           Number
+
+        */
         Task<UserDataModel> GetUserData (UserDataModel e);
+
+        //! Add Function
+        /*
+           To add user data to table
+        */
         Task<UserDataModel> Add(UserDataModel e);
+
+        //! Get Function
+        /*
+           To get string values 
+        */
         Task<string> Get();
     }
 }
