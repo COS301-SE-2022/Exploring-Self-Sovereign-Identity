f50c6e1018d97296f82bb98070ce6cdb3fbd0caf
Rebecca Pillay <u17016534@up.ac.za>
2022-06-07 23:02:49 +0200

    User Data Endpoint (Update)

---
+++ ExploringSelfSovereignIdentityAPI/Queries/ConnectEndpoint/ConnectEndpointUpdateUserDataQuery.cs
@@ -0,0 +1,15 @@
+﻿using ExploringSelfSovereignIdentityAPI.Models.Entity;
+using MediatR;
+
+namespace ExploringSelfSovereignIdentityAPI.Queries.ConnectEndpoint
+{
+    public class ConnectEndpointUpdateUserDataQuery
+    {
+        public record ConnectEndpointUpdateDataQuery : IRequest<UserDataModel>
+        {
+
+        }
+
+    }
+}
+
