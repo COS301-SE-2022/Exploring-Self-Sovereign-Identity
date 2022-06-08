6b15e289e82082aba94fda59359324c9ca4a7d0c
MxolisiMkanzi <u18059326@tuks.co.za>
2022-06-08 21:01:16 +0200

    Add add contract command

---
+++ ExploringSelfSovereignIdentityAPI/Commands/Contract/AddContractCommand.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+using System.Collections.Generic;
+
+namespace ExploringSelfSovereignIdentityAPI.Commands.Contract
+{
+    public class AddContractCommand: IRequest<string>
+    {
+        public string signature { get; set; }
+        public LinkedList<int> attributes { get; set; }
+
+        public AddContractCommand()
+        {
+            attributes = new LinkedList<int>();
+        }
+
+        public AddContractCommand(string signature, LinkedList<int> attributes)
+        {
+            this.signature = signature;
+            this.attributes = attributes;
+        }
+    }
+}
