52268d5c2e0a325262797e233ebdb0d5adfccfb8
MxolisiMkanzi <u18059326@tuks.co.za>
2022-06-08 21:06:48 +0200

    Add add contract endpoint

--- ExploringSelfSovereignIdentityAPI/Controllers/TransactionController.cs
+++ ExploringSelfSovereignIdentityAPI/Controllers/TransactionController.cs
@@ -1,12 +1,34 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ExploringSelfSovereignIdentityAPI.Commands.Contract;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 
 namespace ExploringSelfSovereignIdentityAPI.Controllers
 {
+    [Route("api/[controller]")]
+    [ApiController]
     public class TransactionController : Controller
     {
-        public IActionResult Index()
+        private readonly IMediator mediator;
+
+        public TransactionController(IMediator med)
         {
-            return View();
+            mediator = med;
         }
+        /*
+         [HttpGet("getExamples")]
+        public async Task<string> GetExample()
+        {
+            return await mediator.Send(new ExampleGetQuery());
+        }*/
+
+        [HttpPost("contract")]
+        public async Task<string> addContract([FromBody] AddContractCommand command)
+        {
+            return await mediator.Send(command);
+        }
+        
+
+
     }
 }
