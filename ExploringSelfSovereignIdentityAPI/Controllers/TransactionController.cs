6c995b483aacb223cfe579a7b949727ce65a2e16
MxolisiMkanzi <u18059326@tuks.co.za>
2022-06-08 20:55:54 +0200

    Add transaction controller

---
+++ ExploringSelfSovereignIdentityAPI/Controllers/TransactionController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ExploringSelfSovereignIdentityAPI.Controllers
+{
+    public class TransactionController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
