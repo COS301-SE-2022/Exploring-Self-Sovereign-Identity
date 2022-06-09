b9a70c925e575860ff16cf51fb6777aa74fdeca9
Sboniso Masilela <u10416260@tuks.co.za>
2022-06-09 20:11:08 +0200

    AddTransactionCommand implementation

---
+++ ExploringSelfSovereignIdentityAPI/Commands/Transactions/AddTransactionCommand.cs
@@ -0,0 +1,24 @@
+﻿using ExploringSelfSovereignIdentityAPI.Models.Entity;
+using ExploringSelfSovereignIdentityAPI.Models.Request;
+using MediatR;
+using System;
+using System.Collections.Generic;
+
+namespace ExploringSelfSovereignIdentityAPI.Commands.Transactions
+{
+    public class AddTransactionCommand : IRequest<Transaction>
+    {
+
+        //public string signature { get; set; }
+        public Guid To { get; set; }
+        public Guid From { get; set; }
+        public LinkedList<AddContractRequest> Attributes { get; set; }
+
+        public AddTransactionCommand()
+        {
+            Attributes = new LinkedList<AddContractRequest>();
+        }
+
+
+    }
+}
