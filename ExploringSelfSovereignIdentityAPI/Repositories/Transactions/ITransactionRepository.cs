bcc2927da77f51bf34f8ce8ab4388a3063b5de10
Sboniso Masilela <u10416260@tuks.co.za>
2022-06-09 22:34:01 +0200

    Added transaction Repository

---
+++ ExploringSelfSovereignIdentityAPI/Repositories/Transactions/ITransactionRepository.cs
@@ -0,0 +1,20 @@
+﻿using ExploringSelfSovereignIdentityAPI.Models.Entity;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ExploringSelfSovereignIdentityAPI.Repositories.Transactions
+{
+    public interface ITransactionRepository
+    {
+
+        public  Task<int> AddPendingTransaction(Transaction transaction);
+
+        public  Task<List<Transaction>> GetPastTransactions(Guid id);
+
+        public  Task<List<Transaction>> GetPendingTransactions(Guid id);
+
+        public  Task<int> SaveTransaction(Transaction saveTransaction);
+
+    }
+}
