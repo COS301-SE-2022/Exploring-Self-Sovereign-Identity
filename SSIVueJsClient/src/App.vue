ee4dcaf39593018c0d4a64f2a0a9087212a36cb1
Henri <40567224+H-e-n-r-i@users.noreply.github.com>
2022-06-10 01:22:22 +0200

    updated routing

--- SSIVueJsClient/src/App.vue
+++ SSIVueJsClient/src/App.vue
@@ -1,9 +1,7 @@
 <script setup lang="ts">
 import { RouterView } from "vue-router";
-import PendingTransactionsView from "./views/PendingTransactionsView.vue";
 </script>
 
 <template>
-  <!-- <RouterView /> -->
-  <PendingTransactionsView />
+  <RouterView />
 </template>
