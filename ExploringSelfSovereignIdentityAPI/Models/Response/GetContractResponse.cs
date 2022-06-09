ae2f6d86d1cdf56cddeffae26b738150df7b1084
Sboniso Masilela <u10416260@tuks.co.za>
2022-06-10 00:47:49 +0200

    Added GetContractResponse

--- ExploringSelfSovereignIdentityAPI/Models/Response/GetContractResponse.cs
+++ ExploringSelfSovereignIdentityAPI/Models/Response/GetContractResponse.cs
@@ -1,6 +1,23 @@
-﻿namespace ExploringSelfSovereignIdentityAPI.Models.Response
+﻿using ExploringSelfSovereignIdentityAPI.Models.Request;
+using System;
+using System.Collections.Generic;
+
+namespace ExploringSelfSovereignIdentityAPI.Models.Response
 {
     public class GetContractResponse
     {
+
+        public string Signature { get; set; }
+        public Guid Id { get; set; }
+
+        public LinkedList<AddAttributeRequest> Attributes { get; set; }
+
+        public GetContractResponse()
+        {
+            Attributes = new LinkedList<AddAttributeRequest>();
+        }
+
     }
 }
+
+
