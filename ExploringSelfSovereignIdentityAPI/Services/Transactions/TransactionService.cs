ee066529b0f7cbbda6ec569451185b311c9346b0
Sboniso Masilela <u10416260@tuks.co.za>
2022-06-09 22:36:41 +0200

    Implemented transactional service

---
+++ ExploringSelfSovereignIdentityAPI/Services/Transactions/TransactionService.cs
@@ -0,0 +1,43 @@
+﻿using ExploringSelfSovereignIdentityAPI.Commands.Transactions;
+using ExploringSelfSovereignIdentityAPI.Models.Entity;
+using ExploringSelfSovereignIdentityAPI.Repositories.Transactions;
+using System;
+using System.Collections.Generic;
+
+namespace ExploringSelfSovereignIdentityAPI.Services.Transactions
+{
+    public class TransactionService : ITransactionService
+    {
+        
+        private readonly ITransactionRepository _transactionRepository;
+
+        public TransactionService(TransactionRepository transactionRepository)
+        {
+            _transactionRepository = transactionRepository;
+        }
+
+        
+        public Transaction AddPendingTransaction(AddTransactionCommand pendingTransaction)
+        {
+            //var transaction = new Transaction();
+            //transaction.ContractID
+            //_transactionRepository.AddPendingTransaction(pendingTransaction);
+            throw new NotImplementedException();
+        }
+
+        public Transaction GetPastTransactions(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Transaction> GetPendingTransactions(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Transaction SaveTransaction(SaveTransactionCommand saveTransaction)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
