8160cc9bcbe1a5be5b13b60146ce46c6217d1111
Sboniso Masilela <u10416260@tuks.co.za>
2022-06-09 22:32:51 +0200

    Added GetPast transaction query class

---
+++ ExploringSelfSovereignIdentityAPI/Queries/Example/GetPastTransactionQuery.cs
@@ -0,0 +1,17 @@
+﻿using ExploringSelfSovereignIdentityAPI.Models.Entity;
+using MediatR;
+using System;
+using System.Collections.Generic;
+
+namespace ExploringSelfSovereignIdentityAPI.Queries.Example
+{
+    public class GetPastTransactionQuery : IRequest<List<Transaction>>
+    {
+        public Guid Id { get; set; }
+
+
+
+
+
+    }
+}
