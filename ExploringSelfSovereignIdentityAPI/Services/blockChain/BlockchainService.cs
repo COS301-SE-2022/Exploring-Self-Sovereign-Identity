65457fac26125dcb8dba500341ac2547bd4cb80b
Johan Smit <jsjohansmit6@gmail.com>
2022-07-26 21:58:50 +0200

    Added working createUser

--- ExploringSelfSovereignIdentityAPI/Services/blockChain/BlockchainService.cs
+++ ExploringSelfSovereignIdentityAPI/Services/blockChain/BlockchainService.cs
@@ -8,11 +8,11 @@ namespace ExploringSelfSovereignIdentityAPI.Services.blockChain
 {
    public class BlockchainService: IBlockchainService
     {
-        private readonly Web3 Web3Instance = new Web3("http://127.0.0.1:8545");
+        private readonly Web3 Web3Instance = new Web3("http://127.0.0.1:7545");
 
-        private readonly string senderAddress = "0xeE07Cf444e5044295228083C652aA46F9fefA44A";
+        private readonly string senderAddress = "0xD6059FB76172C8d7f4845c94a0D8b2F74620105b";
 
-        private readonly string contractAddress = "0x38Bfc64aA1d9D8e5DaFb08f252F7619Cd31705B7";
+        private readonly string contractAddress = "0xf84F284bb25bE2Bae65908BD0b63c263EEFDa41D";
 
         private readonly string abi = @"[{'inputs':[{'internalType':'string','name':'_id','type':'string'}],'name':'createUser','outputs':[],'stateMutability':'nonpayable','type':'function'},{'inputs':[{'internalType':'string','name':'_id','type':'string'},{'components':[{'components':[{'internalType':'string','name':'name','type':'string'},{'internalType':'string','name':'value','type':'string'}],'internalType':'struct UserDataContract.Attribute','name':'attribute','type':'tuple'},{'internalType':'bool','name':'isUpdate','type':'bool'}],'internalType':'struct UserDataContract.AttributeUpdate[]','name':'attributes','type':'tuple[]'},{'components':[{'internalType':'string','name':'organization','type':'string'},{'internalType':'bool','name':'isUpdate','type':'bool'},{'components':[{'internalType':'string','name':'name','type':'string'},{'internalType':'string','name':'value','type':'string'}],'internalType':'struct UserDataContract.Attribute[]','name':'attributes','type':'tuple[]'}],'internalType':'struct UserDataContract.CredentialUpdate[]','name':'credentials','type':'tuple[]'}],'name':'updateUser','outputs':[],'stateMutability':'nonpayable','type':'function'},{'inputs':[{'internalType':'string','name':'_id','type':'string'}],'name':'getUserData','outputs':[{'components':[{'internalType':'string','name':'id','type':'string'},{'components':[{'internalType':'string','name':'name','type':'string'},{'internalType':'string','name':'value','type':'string'}],'internalType':'struct UserDataContract.Attribute[]','name':'attributes','type':'tuple[]'},{'components':[{'internalType':'string','name':'organization','type':'string'},{'components':[{'internalType':'string','name':'name','type':'string'},{'internalType':'string','name':'value','type':'string'}],'internalType':'struct UserDataContract.Attribute[]','name':'attributes','type':'tuple[]'}],'internalType':'struct UserDataContract.CredentialResponse[]','name':'credentials','type':'tuple[]'}],'internalType':'struct UserDataContract.UserDataResponse','name':'','type':'tuple'}],'stateMutability':'view','type':'function','constant':true}]";
 
@@ -29,14 +29,12 @@ namespace ExploringSelfSovereignIdentityAPI.Services.blockChain
             try
             {
                 //id, [[["name","Johan"],false],[["surname","Smit"],false],[["age","21"],false]], []
-                string s = "id, [[[\"name\",\"Johan\"],false],[[\"surname\",\"Smit\"],false],[[\"age\",\"21\"],false]], []";
+                //string s = "id, [[[\"name\",\"Johan\"],false],[[\"surname\",\"Smit\"],false],[[\"age\",\"21\"],false]], []";
 
                 var createUserFunction = contract.GetFunction("getUserData");
                 try
                 {
-                    //var gas = await createUserFunction.EstimateGasAsync(senderAddress, null, null, addressTo, value);
-                    //var tx = await createUserFunction.SendTransactionAndWaitForReceiptAsync(senderAddress, gas, null, null, addressTo, value);
-                    return await createUserFunction.SendTransactionAsync(senderAddress, new HexBigInteger(60), new HexBigInteger(60), new HexBigInteger(60), "id");
+                    return await createUserFunction.SendTransactionAsync(senderAddress, new HexBigInteger(900000), null, "id");
                 }
                 catch (Exception e)
                 {
