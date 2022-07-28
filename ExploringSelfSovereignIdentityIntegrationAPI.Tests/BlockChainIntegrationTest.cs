b74b98100fe24fe80b999433b6a594b06726351b
MxolisiMkanzi <u18059326@tuks.co.za>
2022-07-28 20:09:40 +0200

    Added integration test for adding a user

--- ExploringSelfSovereignIdentityIntegrationAPI.Tests/BlockChainIntegrationTest.cs
+++ ExploringSelfSovereignIdentityIntegrationAPI.Tests/BlockChainIntegrationTest.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ExploringSelfSovereignIdentityAPI.Services.blockChain;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,18 @@ using System.Threading.Tasks;
 
 namespace ExploringSelfSovereignIdentityIntegrationAPI.Tests
 {
-    internal class BlockChainIntegrationTest
+    [TestClass]
+    public class BlockChainIntegrationTest
     {
+        public BlockchainService _blockChainService;
+
+        [TestMethod]
+        public async Task TestCreateUser()
+        {
+            string userId = "aaa";
+            string res = await _blockChainService.createUser(userId);
+            Assert.IsNotNull(res);
+            Assert.AreEqual(userId, res);
+        }
     }
 }
