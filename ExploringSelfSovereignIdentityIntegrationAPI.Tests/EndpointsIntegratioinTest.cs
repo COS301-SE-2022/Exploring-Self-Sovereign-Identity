d2ccc4306b3be3e7e367e63448a9ad77de00dad1
Rebecca Pillay <u17016534@up.ac.za>
2022-07-28 22:39:05 +0200

    API Integration Tests

--- ExploringSelfSovereignIdentityIntegrationAPI.Tests/EndpointsIntegratioinTest.cs
+++ ExploringSelfSovereignIdentityIntegrationAPI.Tests/EndpointsIntegratioinTest.cs
@@ -3,16 +3,70 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ExploringSelfSovereignIdentityAPI.Models;
+using ExploringSelfSovereignIdentityAPI.Services.UserDataService;
+
 
 namespace ExploringSelfSovereignIdentityIntegrationAPI.Tests
 {
     [TestClass]
     public class EndpointsIntegratioinTest
     {
-        [TestMethod]
-        public void testMethod()
+        public UserdataService _userDataService;
+        //public IUserDataService _userRepo = new IUserDataService();
+        public EndpointsIntegratioinTest()
         {
+           // _userDataService = new UserdataService ();
+        }
+
+        [TestMethod]
+        public async Task TestRegister()
+        {
+          /*  var webAppFactory = new WebApplicationFactory<UserdataService>();
+            var httpClient = webAppFactory.CreateDefaultClient();
+            var response = await httpClient.GetAsync("");
+            var result = await response.Content.ReadAsStringAsync();
+            Assert.AreEqual("aaa", result);*/ 
 
         }
+
+        [TestMethod]
+        public async Task TestGetUserData()
+        {
+
+           /* var webAppFactory = new WebApplicationFactory<UserdataService>();
+            var httpClient = webAppFactory.CreateDefaultClient();
+            var response = await httpClient.GetAsync("");
+            var result = await response.Content.ReadAsStringAsync();
+            Assert.AreEqual( "aaa" , result );*/ 
+            
+
+        }
+
+        [TestMethod]
+        public async Task TestUpdateAttributes()
+        {
+           /* string userId = "aaa";
+            AttributeBC name = new AttributeBC();
+            name.name = "name";
+            name.value = "TestName";
+            name.index = 0;
+
+            AttributeBC surname = new AttributeBC();
+            surname.name = "surname";
+            surname.value = "TestSurname";
+            surname.index = 1;
+
+            AttributeBC[] attributes = new AttributeBC[2];
+            attributes[0] = name;
+            attributes[1] = surname;
+            string res = await UserdataService.UpdateUserData(userId, attributes);
+
+            Assert.IsNotNull(res);
+            Assert.AreEqual("success", res);*/ 
+
+        }
+
+
     }
 }
