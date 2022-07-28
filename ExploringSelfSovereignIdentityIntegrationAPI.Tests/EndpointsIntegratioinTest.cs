6de245b9137188236543b01d51c1e0bea5de0a9a
MxolisiMkanzi <u18059326@tuks.co.za>
2022-07-28 20:03:38 +0200

    Created Integration test classes

---
+++ ExploringSelfSovereignIdentityIntegrationAPI.Tests/EndpointsIntegratioinTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExploringSelfSovereignIdentityIntegrationAPI.Tests
+{
+    [TestClass]
+    public class EndpointsIntegratioinTest
+    {
+        [TestMethod]
+        public void testMethod()
+        {
+
+        }
+    }
+}
