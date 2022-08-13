3980f0602478fb3882533d601d86879d8fe02dce
Sboniso Masilela <u10416260@tuks.co.za>
2022-08-13 13:01:09 +0200

    Added new function interface

--- ExploringSelfSovereignIdentityAPI/Services/NetheriumBlockChain/IUserDataService.cs
+++ ExploringSelfSovereignIdentityAPI/Services/NetheriumBlockChain/IUserDataService.cs
@@ -10,6 +10,6 @@ namespace ExploringSelfSovereignIdentityAPI.Services.NetheriumBlockChain
         public Task<string> createUser(string id);
         public Task<string> updateAttributes(string id, AttributeBC[] attributes);
         public Task<GetUserDataOutputDTO> getUserData(string id);
-        public Task<GetUserDataOutputDTO> updateUserData(string id, Update update);
+        public Task<GetUserDataOutputDTO> updateUserData( UpdateGen2 update);
     }
 }
