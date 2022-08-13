00e4fc7df5895c42f9c6d85687780e12c8cf2f3b
Sboniso Masilela <u10416260@tuks.co.za>
2022-08-13 13:10:39 +0200

    Implemented CredentialUpdateBaseGen2 function

--- ExploringSelfSovereignIdentityAPI/Services/NetheriumBlockChain/UserDataService.cs
+++ ExploringSelfSovereignIdentityAPI/Services/NetheriumBlockChain/UserDataService.cs
@@ -263,7 +263,17 @@ namespace ExploringSelfSovereignIdentityAPI.Services.NetheriumBlockChain
         public virtual List<Attribute> Attributes { get; set; }
     }
 
+    public partial class CredentialUpdateGen2 : CredentialUpdateBaseGen2 { }
 
+    public class CredentialUpdateBaseGen2
+    {
+        [Parameter("string", "organization", 1)]
+        public virtual string Organization { get; set; }
+        [Parameter("int256", "index", 2)]
+        public virtual int Index { get; set; }
+        [Parameter("tuple[]", "attributes", 3)]
+        public virtual List<Attribute> Attributes { get; set; }
+    }
 
     public partial class Update : UpdateBase { }
 
