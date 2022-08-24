f0945948147a0f93285f3dbcf33bfdfa29747dd9
Rebecca Pillay <u17016534@up.ac.za>
2022-08-24 22:09:32 +0200

    Changes

--- SSIVueJsClient/src/main.ts
+++ SSIVueJsClient/src/main.ts
@@ -4,11 +4,20 @@ import App from "./App.vue";
 import router from "./router";
 import ElementPlus from "element-plus";
 import "element-plus/dist/index.css";
+import 'perfect-scrollbar/css/perfect-scrollbar.css';
+import './styles/reset.css';
+import './styles/global.scss';
+import store, { storeKey } from '@/stores'
+import i18n from './i18n'
+
 
 const app = createApp(App);
 app.use(ElementPlus);
 //* These errors will show however they aren't actual errors
 app.use(createPinia());
 app.use(router);
+app.use(store, storeKey);
+
+app.use(i18n);
 
 app.mount("#app");
