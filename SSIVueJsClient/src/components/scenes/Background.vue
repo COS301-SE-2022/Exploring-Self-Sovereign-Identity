c6320c4e7fbf30f16c59b5d664df3e960a660914
Rebecca Pillay <u17016534@up.ac.za>
2022-08-25 17:54:00 +0200

    background adjustments

---
+++ SSIVueJsClient/src/components/scenes/Background.vue
@@ -0,0 +1,25 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <div class="avatar-background" :style="{ background: props.color }"></div>
+</template>
+
+<script lang="ts" setup>
+import { type AvatarOption } from "@/types";
+
+export interface BackgroundProps {
+  color: AvatarOption["background"]["color"];
+}
+
+const props = defineProps<BackgroundProps>();
+</script>
+
+<style lang="scss" scoped>
+.avatar-background {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  transition: background-color 0.1s;
+}
+</style>
