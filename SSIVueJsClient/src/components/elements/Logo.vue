42b13feb4fd525204820d586897b3f8f7ce5e735
Rebecca Pillay <u17016534@up.ac.za>
2022-08-25 14:34:55 +0200

    Adding page elements

---
+++ SSIVueJsClient/src/components/elements/Logo.vue
@@ -0,0 +1,16 @@
+<template>
+  <img
+    style="width: 40px; height: 40px"
+    :src="LogoSvg"
+    :style="{ width: `${props.size}rem`, height: `${props.size}rem` }"
+    alt="logo"
+  />
+</template>
+
+<script lang="ts" setup>
+import LogoSvg from '@/assets/COD6.png'
+
+const props = withDefaults(defineProps<{ size?: number }>(), {
+  size: 6.5,
+})
+</script>
