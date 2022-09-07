fb1ca11a6b0e0b0334d7898f05bcb9750bf43409
Sboniso Masilela <u10416260@tuks.co.za>
2022-09-07 20:21:36 +0200

    Implemented Encryption function

--- ExploringSelfSovereignIdentityAPI/Services/Encryption/EncryptionService.cs
+++ ExploringSelfSovereignIdentityAPI/Services/Encryption/EncryptionService.cs
@@ -38,11 +38,31 @@ namespace ExploringSelfSovereignIdentityAPI.Services.Encryption
             byte[] iv = new byte[16];
             byte[] array;
 
+                using (Aes aes = Aes.Create())
+                {
+                    aes.Key = Encoding.UTF8.GetBytes(key);
+                    aes.IV = iv;
 
+                    ICryptoTransform encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
 
-            return "";
+                    using (MemoryStream memoryStream = new MemoryStream())
+                    {
+                        using (CryptoStream cryptoStream = new CryptoStream((Stream)memoryStream, encryptor, CryptoStreamMode.Write))
+                        {
+                            using (StreamWriter streamWriter = new StreamWriter((Stream)cryptoStream))
+                            {
+                                streamWriter.Write(plainText);
+                            }
+
+                            array = memoryStream.ToArray();
+                        }
+                    }
+                }
+
+                return Convert.ToBase64String(array);
 
         }
+
 
 
     }
