eb99a95c8fa7cc048a0d123b3457303d716c089c
MxolisiMkanzi <u18059326@tuks.co.za>
2022-09-07 20:25:54 +0200

    Updated decryption function

--- ExploringSelfSovereignIdentityAPI/Services/Encryption/EncryptionService.cs
+++ ExploringSelfSovereignIdentityAPI/Services/Encryption/EncryptionService.cs
@@ -14,23 +14,20 @@ namespace ExploringSelfSovereignIdentityAPI.Services.Encryption
             byte[] iv = new byte[16];
             byte[] buffer = Convert.FromBase64String(cipherText);
 
-            using (Aes aes = Aes.Create())
-            {
-                aes.Key = Encoding.UTF8.GetBytes(key);
-                aes.IV = iv;
-                ICryptoTransform decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
+            Aes aes = Aes.Create();
 
-                using (MemoryStream memoryStream = new MemoryStream(buffer))
-                {
-                    using (CryptoStream cryptoStream = new CryptoStream((Stream)memoryStream, decryptor, CryptoStreamMode.Read))
-                    {
-                        using (StreamReader streamReader = new StreamReader((Stream)cryptoStream))
-                        {
-                            return streamReader.ReadToEnd();
-                        }
-                    }
-                }
-            }
+            aes.Key = Encoding.UTF8.GetBytes(key);
+            aes.IV = iv;
+
+            ICryptoTransform decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
+
+            MemoryStream memoryStream = new MemoryStream(buffer);
+
+            CryptoStream cryptoStream = new CryptoStream((Stream)memoryStream, decryptor, CryptoStreamMode.Read);
+
+            StreamReader streamReader = new StreamReader((Stream)cryptoStream);
+
+            return streamReader.ReadToEnd();
         }
 
         public static string EncryptString(string key, string plainText)
