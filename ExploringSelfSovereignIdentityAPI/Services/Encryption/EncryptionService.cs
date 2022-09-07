1cb9f148f42b082312a3ca44e90c6b0cc5598f26
MxolisiMkanzi <u18059326@tuks.co.za>
2022-09-07 20:18:40 +0200

    Defined decryption function

--- ExploringSelfSovereignIdentityAPI/Services/Encryption/EncryptionService.cs
+++ ExploringSelfSovereignIdentityAPI/Services/Encryption/EncryptionService.cs
@@ -1,4 +1,9 @@
-﻿namespace ExploringSelfSovereignIdentityAPI.Services.Encryption
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace ExploringSelfSovereignIdentityAPI.Services.Encryption
 {
     public class EncryptionService
     {
@@ -6,7 +11,26 @@
         private static string staticKey = "StaticKey";
         public string DecryptString(string key, string cipherText)
         {
-            return "";
+            byte[] iv = new byte[16];
+            byte[] buffer = Convert.FromBase64String(cipherText);
+
+            using (Aes aes = Aes.Create())
+            {
+                aes.Key = Encoding.UTF8.GetBytes(key);
+                aes.IV = iv;
+                ICryptoTransform decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
+
+                using (MemoryStream memoryStream = new MemoryStream(buffer))
+                {
+                    using (CryptoStream cryptoStream = new CryptoStream((Stream)memoryStream, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (StreamReader streamReader = new StreamReader((Stream)cryptoStream))
+                        {
+                            return streamReader.ReadToEnd();
+                        }
+                    }
+                }
+            }
         }
 
         public static string EncryptString(string key, string plainText)
