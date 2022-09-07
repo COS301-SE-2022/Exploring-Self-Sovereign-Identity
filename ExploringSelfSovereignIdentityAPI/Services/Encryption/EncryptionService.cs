dfed2558a351df4ab97b0860f66779132c517e25
MxolisiMkanzi <u18059326@tuks.co.za>
2022-09-07 20:07:46 +0200

    Added decryption function definition

--- ExploringSelfSovereignIdentityAPI/Services/Encryption/EncryptionService.cs
+++ ExploringSelfSovereignIdentityAPI/Services/Encryption/EncryptionService.cs
@@ -2,5 +2,11 @@
 {
     public class EncryptionService
     {
+
+        private static string staticKey = "StaticKey";
+        public string DecryptString(string key, string cipherText)
+        {
+            return "";
+        }
     }
 }
