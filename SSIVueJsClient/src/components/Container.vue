14d3d2169fb209cba9cc056a2eec77204af4a549
Rebecca Pillay <u17016534@tuks.co.za>
2022-09-19 10:51:36 +0100

    Avatar Changes

--- SSIVueJsClient/src/components/Container.vue
+++ SSIVueJsClient/src/components/Container.vue
@@ -1,66 +1,64 @@
 <!-- eslint-disable vue/multi-word-component-names -->
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { onMounted, onUnmounted } from 'vue'
+import { useSider } from '@/hooks'
+import { SCREEN } from '@/utils/constant'
+export default defineComponent({
+  setup() {
+    const { isCollapsed, openSider, closeSider } = useSider()
+
+    function handleWindowResize() {
+      if (window.innerWidth <= SCREEN.lg) {
+        closeSider()
+      } else {
+        openSider()
+      }
+    }
+
+    onMounted(() => {
+      void (function () {
+        const throttle = function (
+          type: string,
+          customEventName: string,
+          obj: Window
+        ) {
+          obj = obj || window
+          let running = false
+          const func = () => {
+            if (running) {
+              return
+            }
+            running = true
+            requestAnimationFrame(() => {
+              obj.dispatchEvent(new CustomEvent(customEventName))
+              running = false
+            })
+          }
+          obj.addEventListener(type, func)
+        }
+        throttle('resize', 'optimizedResize', window)
+      })()
+
+      window.addEventListener('optimizedResize', handleWindowResize)
+    })
+
+    onUnmounted(() => {
+      window.removeEventListener('optimizedResize', handleWindowResize)
+    })
+
+    return {
+      handleWindowResize,
+      isCollapsed, 
+    }
+  },
+})
+</script>
 <template>
   <section class="container" :class="{ full: isCollapsed }">
     <slot />
   </section>
 </template>
-<script lang="ts">
-import { defineComponent } from 'vue'
-export default defineComponent({
-  data() {
-    return {}
-  },
-  components: {},
-  methods: {},
-})
-</script>
-<script lang="ts" setup>
-import { onMounted, onUnmounted } from 'vue'
-
-import { useSider } from '@/hooks'
-import { SCREEN } from '@/utils/constant'
-
-const { isCollapsed, openSider, closeSider } = useSider()
-
-function handleWindowResize() {
-  if (window.innerWidth <= SCREEN.lg) {
-    closeSider()
-  } else {
-    openSider()
-  }
-}
-
-onMounted(() => {
-  void (function () {
-    const throttle = function (
-      type: string,
-      customEventName: string,
-      obj: Window
-    ) {
-      obj = obj || window
-      let running = false
-      const func = () => {
-        if (running) {
-          return
-        }
-        running = true
-        requestAnimationFrame(() => {
-          obj.dispatchEvent(new CustomEvent(customEventName))
-          running = false
-        })
-      }
-      obj.addEventListener(type, func)
-    }
-    throttle('resize', 'optimizedResize', window)
-  })()
-
-  window.addEventListener('optimizedResize', handleWindowResize)
-})
-
-onUnmounted(() => {
-  window.removeEventListener('optimizedResize', handleWindowResize)
-})
-</script>
 
 <style lang="scss" scoped>
 @use 'src/styles/var';
