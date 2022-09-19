4e424f41da18561ba98c1ee3bf76263be2fff525
Johan Smit <jsjohansmit6@gmail.com>
2022-09-19 12:11:14 +0200

    Added Tests  for Market Place

--- LocalBlockchain/migrations/1_user_data_migration.js
+++ LocalBlockchain/migrations/1_user_data_migration.js
@@ -1,7 +1,9 @@
 // Help Truffle find `UserDataCOntract.sol` in the `/contracts` directory
 const UserDataContract = artifacts.require("UserDataContract");
+const MarketPlace = artifacts.require("MarketPlace");
 
 module.exports = function(deployer) {
   // Command Truffle to deploy the Smart Contract
   deployer.deploy(UserDataContract);
+  deployer.deploy(MarketPlace);
 };
--- LocalBlockchain/test/UserDataContractTest.js
+++ LocalBlockchain/test/UserDataContractTest.js
@@ -1,6 +1,7 @@
-const { assert } = require("chai");
+const { assert, use } = require("chai");
 
 const UserDataContract = artifacts.require("./UserDataContract.sol");
+const MarketPlace = artifacts.require("./MarketPlace.sol");
 
 require("chai")
     .use(require("chai-as-promised"))
@@ -11,19 +12,21 @@ require("chai")
 contract('UserDataContract', ([contractOwner, secondAddress, thirdAddress]) => {
 
     let udc;
+    // let mkp;
     let id = "AAA-BBB-CCC";
     var result;
 
     /* Attach deployed smart contract to udc variable. */
     before(async () => {
         udc = await UserDataContract.deployed();
+        // mkp = await MarketPlace.deployed();
     });
 
     /* Test to see if deployement is successful. */
     describe("Deployment", () => {
 
-            it("Deploys Successfully", async () => {
-            const address = await udc.address
+        it("Deploys Successfully", async () => {
+            var address = await udc.address
 
             assert.notEqual(address, '');
             assert.notEqual(address, undefined);
@@ -32,103 +35,179 @@ contract('UserDataContract', ([contractOwner, secondAddress, thirdAddress]) => {
         });
     });
 
-    describe("UserData", async() => {
+    // describe("UserData", async() => {
 
-        it("Creates a user successfully.", async () => {
-            
-            await udc.createUser(id);
-            result = await udc.getUserData(id);
+    //     it("Creates a user successfully.", async () => {
 
-            assert.equal(result.id, id);
-            assert.equal(result.attributes.length, 0);
+    //         await udc.createUser(id);
+    //         result = await udc.getUserData(id);
 
-        });
+    //         assert.equal(result.id, id);
+    //         assert.equal(result.attributes.length, 0);
 
-        it("Inserts new Attributes and Credentials successfully.", async () => {
+    //     });
 
-            await udc.updateUser([id,[ [["name","Johan"],-1], [["surname","Smit"],-1] ], [ ["UP",-1,[ ["student_number","u20502126"],["password","oopsiedaisy"] ]] ]]);
-            result = await udc.getUserData(id);
+    //     it("Inserts new Attributes and Credentials successfully.", async () => {
 
-            /* Attributes */
-            assert.equal(result.attributes.length, 2);
-            assert.equal(result.attributes[0].name, "name");
-            assert.equal(result.attributes[1].value, "Smit");
+    //         await udc.updateUser([id,[ [["name","Johan"],-1], [["surname","Smit"],-1] ], [ ["UP",-1,[ ["student_number","u20502126"],["password","oopsiedaisy"] ]] ]]);
+    //         result = await udc.getUserData(id);
 
-            /* Credentials */
-            assert.equal(result.credentials.length, 1);
-            assert.equal(result.credentials[0].attributes.length, 2);
-            assert.equal(result.credentials[0].organization, "UP");
-            assert.equal(result.credentials[0].attributes[0].value, "u20502126");
-            assert.equal(result.credentials[0].attributes[1].name, "password");
-        });
+    //         /* Attributes */
+    //         assert.equal(result.attributes.length, 2);
+    //         assert.equal(result.attributes[0].name, "name");
+    //         assert.equal(result.attributes[1].value, "Smit");
 
-        it("Updates existing Attributes and Credentials successfully.", async () => {
+    //         /* Credentials */
+    //         assert.equal(result.credentials.length, 1);
+    //         assert.equal(result.credentials[0].attributes.length, 2);
+    //         assert.equal(result.credentials[0].organization, "UP");
+    //         assert.equal(result.credentials[0].attributes[0].value, "u20502126");
+    //         assert.equal(result.credentials[0].attributes[1].name, "password");
+    //     });
 
-            await udc.updateUser([id,[ [["alias","Johan"],0], [["age","22"],-1] ], [ ["UP",0,[ ["password","oowoo"] ]] ]]);
-            result = await udc.getUserData(id);
+    //     it("Updates existing Attributes and Credentials successfully.", async () => {
 
-            /* Attributes */
-            assert.equal(result.attributes.length, 3);
-            assert.equal(result.attributes[0].name, "alias");
-            assert.equal(result.attributes[0].value, "Johan");
-            assert.equal(result.attributes[1].name, "surname");
-            assert.equal(result.attributes[1].value, "Smit");
-            assert.equal(result.attributes[2].name, "age");
-            assert.equal(result.attributes[2].value, "22");
+    //         await udc.updateUser([id,[ [["alias","Johan"],0], [["age","22"],-1] ], [ ["UP",0,[ ["password","oowoo"] ]] ]]);
+    //         result = await udc.getUserData(id);
 
-            /* Credentials */
-            assert.equal(result.credentials.length, 1);
-            assert.equal(result.credentials[0].attributes.length, 2);
-            assert.equal(result.credentials[0].organization, "UP");
-            assert.equal(result.credentials[0].attributes[0].value, "u20502126");
-            assert.equal(result.credentials[0].attributes[1].name, "password");
-            assert.equal(result.credentials[0].attributes[1].value, "oowoo");
-        });
+    //         /* Attributes */
+    //         assert.equal(result.attributes.length, 3);
+    //         assert.equal(result.attributes[0].name, "alias");
+    //         assert.equal(result.attributes[0].value, "Johan");
+    //         assert.equal(result.attributes[1].name, "surname");
+    //         assert.equal(result.attributes[1].value, "Smit");
+    //         assert.equal(result.attributes[2].name, "age");
+    //         assert.equal(result.attributes[2].value, "22");
 
+    //         /* Credentials */
+    //         assert.equal(result.credentials.length, 1);
+    //         assert.equal(result.credentials[0].attributes.length, 2);
+    //         assert.equal(result.credentials[0].organization, "UP");
+    //         assert.equal(result.credentials[0].attributes[0].value, "u20502126");
+    //         assert.equal(result.credentials[0].attributes[1].name, "password");
+    //         assert.equal(result.credentials[0].attributes[1].value, "oowoo");
+    //     });
+
+    // });
+
+    // describe("Transactions", async() => {
+
+    //     it("Adds new TransactionRequests successfully.", async () => {
+
+    //         await udc.createUser("from");
+    //         await udc.newTransactionRequest([["alias"], ["from",id,"2022/08/03","Please give me your name!", "pending"]]);
+    //         result = await udc.getUserData(id);
+
+    //         //console.log(result.transactionRequests);
+
+    //         assert.equal(result.id, id);
+    //         assert.equal(result.transactionRequests[0].stamp.toID, id);
+    //         assert.equal(result.transactionRequests[0].stamp.fromID, "from");
+    //         assert.equal(result.transactionRequests[0].attributes[0], "alias");
+    //     });
+
+    //     it("Approves stage A, B and C Transactions.", async () => {
+
+    //         await udc.approveTransactionStageA(id, 0);
+    //         result = await udc.approveTransactionStageB(id, 0);
+    //         await udc.approveTransactionStageC("from", [[["alias","Johan"]], ["from",id,"2022/08/03","Please give me your name!", "approved"]]);
+
+    //         //console.log(result);
+
+    //         assert.equal(result.stamp.status, "approved");
+    //         assert.equal(result.attributes[0].name, "alias");
+    //         assert.equal(result.attributes[0].value, "Johan");
+
+    //         result = await udc.getUserData("from");
+
+    //         console.log(result.approvedTransactions[0].attributes);
+
+    //         assert.equal(result.approvedTransactions[0].stamp.status, "approved");
+    //         assert.equal(result.approvedTransactions[0].attributes[0].value, "Johan");
+    //     });
+    // });
+
+    // describe("Session Credentials", async () => {
+    //     it ("Gets a credential", async () => {
+    //         //console.log(await udc.getUserData(id));
+    //         result = await udc.getCredential(id, "UP");
+    //         //console.log(result);
+    //     })
+    // });
+
+
+});
+
+contract("MarketPlace", ([contractOwner, secondAddress, thirdAddress]) => {
+    let mkp;
+    var result;
+
+    /* Attach deployed smart contract to udc variable. */
+    before(async () => {
+        mkp = await MarketPlace.deployed();
     });
 
-    describe("Transactions", async() => {
-        
-        it("Adds new TransactionRequests successfully.", async () => {
+    /* Test to see if deployement is successful. */
+    describe("Deployment", () => {
 
-            await udc.createUser("from");
-            await udc.newTransactionRequest([["alias"], ["from",id,"2022/08/03","Please give me your name!", "pending"]]);
-            result = await udc.getUserData(id);
+        it("Deploys Successfully", async () => {
+            var address = await mkp.address
 
-            //console.log(result.transactionRequests);
-
-            assert.equal(result.id, id);
-            assert.equal(result.transactionRequests[0].stamp.toID, id);
-            assert.equal(result.transactionRequests[0].stamp.fromID, "from");
-            assert.equal(result.transactionRequests[0].attributes[0], "alias");
+            assert.notEqual(address, '');
+            assert.notEqual(address, undefined);
+            assert.notEqual(address, null);
+            assert.notEqual(address, 0x0);
         });
 
-        it("Approves stage A, B and C Transactions.", async () => {
+        var username = "Google";
+        var password = "12345";
 
-            await udc.approveTransactionStageA(id, 0);
-            result = await udc.approveTransactionStageB(id, 0);
-            await udc.approveTransactionStageC("from", [[["alias","Johan"]], ["from",id,"2022/08/03","Please give me your name!", "approved"]]);
+        describe("Market Place", async () => {
+            it("Creates an organization", async () => {
 
-            //console.log(result);
+                await mkp.createOrganization([username, password]);
+                result = await mkp.getOrganization([username, password]);
 
-            assert.equal(result.stamp.status, "approved");
-            assert.equal(result.attributes[0].name, "alias");
-            assert.equal(result.attributes[0].value, "Johan");
+                //console.log(result);
+                assert.equal(result.id, "Google");
+            });
             
-            result = await udc.getUserData("from");
+            it("Adds Data Packs", async () => {
+                await mkp.addDataPack([
+                    username,
+                    username+"1",
+                    1,
+                    [
+                        "name",
+                        "surname"
+                    ]
+                ]);
 
-            console.log(result.approvedTransactions[0].attributes);
+                result = await mkp.getOrganization([username, password]);
 
-            assert.equal(result.approvedTransactions[0].stamp.status, "approved");
-            assert.equal(result.approvedTransactions[0].attributes[0].value, "Johan");
+                //console.log(result);
+            });
+
+            it("Buys data", async () => {
+                await mkp.buyData([
+                    "aaa",
+                    username,
+                    username+"1",
+                    [
+                        [
+                            "name",
+                            "johan"
+                        ],
+                        [
+                            "surname",
+                            "smit"
+                        ]
+                    ]
+                ]);
+
+                result = await mkp.getOrganization([username, password]);
+                console.log(result.packs[0].received[0]);
+            });
         });
-    });
-
-    describe("Session Credentials", async () => {
-        it ("Gets a credential", async () => {
-            //console.log(await udc.getUserData(id));
-            result = await udc.getCredential(id, "UP");
-            //console.log(result);
-        })
     });
 });
