c6500a666dbc00e7fa1df44db0782f2c01cdfb59
Rebecca Pillay <u17016534@tuks.co.za>
2022-09-19 10:53:07 +0100

    Avatar Changes

--- SSIVueJsClient/src/components/ConfettiCanvas.vue
+++ SSIVueJsClient/src/components/ConfettiCanvas.vue
@@ -1,9 +1,3 @@
-<template>
-  <canvas
-    id="confetti"
-    style="position: absolute; top: 0; left: 0; width: 100%; height: 100%"
-  ></canvas>
-</template>
 <script lang="ts">
 import { defineComponent } from 'vue'
 export default defineComponent({
@@ -14,3 +8,10 @@ export default defineComponent({
   methods: {},
 })
 </script>
+
+<template>
+  <canvas
+    id="confetti"
+    style="position: absolute; top: 0; left: 0; width: 100%; height: 100%"
+  ></canvas>
+</template>
