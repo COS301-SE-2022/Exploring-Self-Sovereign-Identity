3876c5bf4493d7765658585746d13f528e3c846d
MxolisiMkanzi <u18059326@tuks.co.za>
2022-09-23 22:55:47 +0200

    Refactor function to get attributes

---
+++ ExploringSelfSovereignIdentityAPI/Models/Function/GetAttributesTransactionFunction.cs
@@ -0,0 +1,18 @@
+﻿using ExploringSelfSovereignIdentityAPI.Models.Entity;
+using Nethereum.ABI.FunctionEncoding.Attributes;
+using Nethereum.Contracts;
+using System.Collections.Generic;
+
+namespace ExploringSelfSovereignIdentityAPI.Models.Function
+{
+    public partial class GetAttributesTransactionFunction : GetAttributesTransactionFunctionBase { }
+
+    [Function("getAttributesTransaction", typeof(GetAttributesTransactionOutputDTO))]
+    public class GetAttributesTransactionFunctionBase : FunctionMessage
+    {
+        [Parameter("string", "_id", 1)]
+        public virtual string Id { get; set; }
+        [Parameter("tuple[]", "attributes", 2)]
+        public virtual List<Attribute> Attributes { get; set; }
+    }
+}
