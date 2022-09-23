8cf6eac26ee39d8d0354973a307f82e7383681b1
MxolisiMkanzi <u18059326@tuks.co.za>
2022-09-23 22:59:31 +0200

    Add get Credential response classes

---
+++ ExploringSelfSovereignIdentityAPI/Models/Response/CredentialResponse.cs
@@ -0,0 +1,16 @@
+﻿using ExploringSelfSovereignIdentityAPI.Models.Entity;
+using Nethereum.ABI.FunctionEncoding.Attributes;
+using System.Collections.Generic;
+
+namespace ExploringSelfSovereignIdentityAPI.Models.Response
+{
+    public partial class CredentialResponse : CredentialResponseBase { }
+
+    public class CredentialResponseBase
+    {
+        [Parameter("string", "organization", 1)]
+        public virtual string Organization { get; set; }
+        [Parameter("tuple[]", "attributes", 2)]
+        public virtual List<Attribute> Attributes { get; set; }
+    }
+}
---
+++ ExploringSelfSovereignIdentityAPI/Models/Response/GetCredentialTransactionOutputDTO.cs
@@ -0,0 +1,13 @@
+﻿using Nethereum.ABI.FunctionEncoding.Attributes;
+
+namespace ExploringSelfSovereignIdentityAPI.Models.Response
+{
+    public partial class GetCredentialTransactionOutputDTO : GetCredentialTransactionOutputDTOBase { }
+
+    [FunctionOutput]
+    public class GetCredentialTransactionOutputDTOBase : IFunctionOutputDTO
+    {
+        [Parameter("tuple", "", 1)]
+        public virtual CredentialResponse ReturnValue1 { get; set; }
+    }
+}
