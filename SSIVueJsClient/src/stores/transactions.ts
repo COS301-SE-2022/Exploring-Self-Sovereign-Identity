b2d9e135df1ec783c423f12d1fa1afceb51b2b87
Henri <hjvdgrijp@gmail.com>
2022-09-23 10:04:31 +0200

    updated packages and created transaction store

--- SSIVueJsClient/src/stores/transactions.ts
+++ SSIVueJsClient/src/stores/transactions.ts
@@ -1,14 +1,43 @@
 import { defineStore } from "pinia";
+import { userDataStore } from "@/stores/userData";
 import axios from "axios";
 
-export const transactionsStore = defineStore("transactions", {
-  state: () => ({
-    api: axios.create({
-      baseURL: "http://localhost:5000",
-      timeout: 20000,
-      headers: {
-        "Content-Type": "application/json",
-      },
-    }),
-  }),
+export const transactionsStore = defineStore("transactions", () => {
+  const api = axios.create({
+    baseURL: "http://localhost:5000",
+    timeout: 20000,
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  const userData = userDataStore();
+
+  return { api, userData };
 });
+
+export interface transactionRequests {
+  attributes: string[];
+  stamp: {
+    fromID: string;
+    toID: string;
+    date: string;
+    message: string;
+    status: string;
+  };
+}
+[];
+
+export interface approvedTransactions {
+  attributes: {
+    name: string;
+    value: string;
+  }[];
+  stamp: {
+    fromID: string;
+    toID: string;
+    date: string;
+    message: string;
+    status: string;
+  };
+}
+[];
