2c37afd8b5ea1d9adf8e8f8dc6699ffe64cd02e7
MxolisiMkanzi <u18059326@tuks.co.za>
2022-09-23 23:06:08 +0200

    Add getCredentialTransactionFunction

---
+++ ExploringSelfSovereignIdentityAPI/Models/Function/GetCredentialTransactionFunction.cs
@@ -0,0 +1,17 @@
+﻿using ExploringSelfSovereignIdentityAPI.Models.Response;
+using Nethereum.ABI.FunctionEncoding.Attributes;
+using Nethereum.Contracts;
+
+namespace ExploringSelfSovereignIdentityAPI.Models.Function
+{
+    public partial class GetCredentialTransactionFunction : GetCredentialTransactionFunctionBase { }
+
+    [Function("getCredentialTransaction", typeof(GetCredentialTransactionOutputDTO))]
+    public class GetCredentialTransactionFunctionBase : FunctionMessage
+    {
+        [Parameter("string", "_id", 1)]
+        public virtual string Id { get; set; }
+        [Parameter("string", "organization", 2)]
+        public virtual string Organization { get; set; }
+    }
+}
