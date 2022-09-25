5abd54404482ad1eb78f485badbbf19317f10d3f
Sboniso Masilela <u10416260@tuks.co.za>
2022-09-25 08:59:12 +0200

    Implemented test for TestnewTransactionRequest

--- ExploringSelfSovereignIdentityIntegrationAPI.Tests/UserDataIntegrationTest.cs
+++ ExploringSelfSovereignIdentityIntegrationAPI.Tests/UserDataIntegrationTest.cs
@@ -142,5 +142,44 @@ namespace ExploringSelfSovereignIdentityIntegrationAPI.Tests
 
 
 
+        [TestMethod]
+        public async Task TestnewTransactionRequest()
+        {
+
+            TransactionRequest request = new TransactionRequest();
+            request.Stamp.ToID = "1234";
+            request.Stamp.FromID = "4321";
+            request.Stamp.Message = "Test";
+            request.Stamp.Date = "Date";
+            request.Stamp.Status = "Status";
+
+            List<string> attrs = new List<string>();
+
+            for (int i = 0; i < request.Attributes.Count; i++)
+            {
+                attrs.Add(request.Attributes[i]);
+            }
+
+
+            try
+            {
+                if (request != null)
+                {
+                    string res = await _userDataService.newTransactionRequest(request);
+                    Assert.IsNotNull(res);
+                    Assert.IsInstanceOfType(res, typeof(string));
+                    Assert.AreEqual(res, request);
+                }
+
+            }
+            catch (Exception e)
+            {
+
+            }
+
+        }
+
+
+
     }
 }
