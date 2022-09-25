6581ead8895c21fdc62db4af1084511fd00f72f9
Sboniso Masilela <u10416260@tuks.co.za>
2022-09-25 17:32:02 +0200

    Implemented Create request handler

---
+++ ExploringSelfSovereignIdentityAPI/Handlers/CreateRequestHandler.cs
@@ -0,0 +1,25 @@
+﻿using ExploringSelfSovereignIdentityAPI.Commands;
+using ExploringSelfSovereignIdentityAPI.Services.NetheriumBlockChain;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ExploringSelfSovereignIdentityAPI.Handlers
+{
+
+    public class CreateRequestHandler : IRequestHandler<CreateRequestCommand, string>
+    {
+        private readonly IUserDataService _service;
+
+        public CreateRequestHandler(IUserDataService service)
+        {
+            _service = service;
+        }
+        public async Task<string> Handle(CreateRequestCommand request, CancellationToken cancellationToken)
+        {
+
+            return await _service.createUser(request.id);
+      
+        }
+    }
+}
