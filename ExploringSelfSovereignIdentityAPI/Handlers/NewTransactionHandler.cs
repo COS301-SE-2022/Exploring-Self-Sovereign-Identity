231018a4750520bb6026c80f496b11cab0d84f06
MxolisiMkanzi <u18059326@tuks.co.za>
2022-09-25 17:25:24 +0200

    Add handler for new transactions

---
+++ ExploringSelfSovereignIdentityAPI/Handlers/NewTransactionHandler.cs
@@ -0,0 +1,23 @@
+﻿using ExploringSelfSovereignIdentityAPI.Commands;
+using ExploringSelfSovereignIdentityAPI.Services.NetheriumBlockChain;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ExploringSelfSovereignIdentityAPI.Handlers
+{
+    public class NewTransactionHandler : IRequestHandler<TransactionRequest, string>
+    {
+        private readonly IUserDataService _service;
+
+        public NewTransactionHandler(IUserDataService service)
+        {
+            _service = service;
+        }
+        public async Task<string> Handle(TransactionRequest request, CancellationToken cancellationToken)
+        {
+
+            return await _service.newTransactionRequest(request);
+        }
+    }
+}
