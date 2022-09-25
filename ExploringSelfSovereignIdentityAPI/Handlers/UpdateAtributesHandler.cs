a4ad9a7ed3c5e111eee36fa4ac2bff9712947751
Sboniso Masilela <u10416260@tuks.co.za>
2022-09-25 18:06:44 +0200

    Implemented update attribute handler

---
+++ ExploringSelfSovereignIdentityAPI/Handlers/UpdateAtributesHandler.cs
@@ -0,0 +1,36 @@
+﻿using ExploringSelfSovereignIdentityAPI.Commands;
+using ExploringSelfSovereignIdentityAPI.Models.Response;
+using ExploringSelfSovereignIdentityAPI.Services.NetheriumBlockChain;
+using MediatR;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using ExploringSelfSovereignIdentityAPI.Models.Entity;
+using Attribute = ExploringSelfSovereignIdentityAPI.Models.Entity.Attribute;
+
+namespace ExploringSelfSovereignIdentityAPI.Handlers
+{
+    public class UpdateAtributesHandler
+    {
+    }
+
+    public class UpdateAtributesHandler : IRequestHandler<UpdateGen2, UserDataResponse>
+    {
+
+        private readonly IUserDataService _service;
+
+        private UserDataResponse response = new UserDataResponse();
+
+        public UpdateAtributesHandler(IUserDataService service)
+        {
+            _service = service;
+        }
+        public async Task<UserDataResponse> Handle(UserDataResponse request, CancellationToken cancellationToken)
+        {
+
+
+
+
+        }
+    }
+}
