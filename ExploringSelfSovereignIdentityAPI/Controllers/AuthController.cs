a16a4baab8bc62539ef043e50e36d93267cff6d2
MxolisiMkanzi <u18059326@tuks.co.za>
2022-09-28 13:57:38 +0200

    Added auth controller with mediator

---
+++ ExploringSelfSovereignIdentityAPI/Controllers/AuthController.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExploringSelfSovereignIdentityAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : Controller
+    {
+        private readonly IMediator mediator;
+        public AuthController(IMediator med)
+        {
+            mediator = med;
+        }
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
