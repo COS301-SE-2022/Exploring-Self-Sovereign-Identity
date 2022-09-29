63601dd412117e73e933002372797dbf911ba67f
MxolisiMkanzi <u18059326@tuks.co.za>
2022-09-29 09:48:50 +0200

    Update token duration

--- ExploringSelfSovereignIdentityAPI/Services/Auth/AuthService.cs
+++ ExploringSelfSovereignIdentityAPI/Services/Auth/AuthService.cs
@@ -52,7 +52,7 @@ namespace ExploringSelfSovereignIdentityAPI.Services.Auth
             var token = new JwtSecurityToken(_config["Jwt:Issuer"],
                 _config["Jwt:Audiance"],
                 claims,
-                expires: DateTime.Now.AddSeconds(15),
+                expires: DateTime.Now.AddHours(1),
                 signingCredentials: credentials);
 
             return new JwtSecurityTokenHandler().WriteToken(token); 
